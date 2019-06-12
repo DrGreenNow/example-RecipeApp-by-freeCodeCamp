bec2633a462c372e3f7c474c986d08a52792ed87
DrGreenNow <AndGreNik@gmail.com>
2019-06-12 12:40:57 +0300

    before pushing on Github

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import Form from "./components/Form";
+import Recipes from "./components/Recipes";
+
+const API_KEY = "b8eecf1a9ec728cd71909cf61994cb8e";
+
+class App extends Component {
+  state = {
+    recipes: []
+  };
+  
+  getRecipe = async (e) => {
+    const recipeName = e.target.elements.recipeName.value;
+    e.preventDefault();
+    const api_call = await fetch(
+      `https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&page=2`
+    );
+    const data = await api_call.json();
+    this.setState({ recipes: data.recipes });
+    console.log(this.state.recipes);
+  };
+
+  componentDidMount = () => {
+    const json = localStorage.getItem("recipes");
+    const recipes = JSON.parse(json);
+    this.setState({ recipes: recipes });
+  }
+
+  componentDidUpdate = () => {
+    const recipes = JSON.stringify(this.state.recipes);
+    localStorage.setItem("recipes", recipes);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Recipe Search</h1>
+        </header>
+        <Form getRecipe={this.getRecipe} />
+        <Recipes recipes={this.state.recipes} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Recipe.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const API_KEY = "b8eecf1a9ec728cd71909cf61994cb8e";
+
+class Recipe extends React.Component {
+  state = {
+    activeRecipe: []
+  };
+
+  componentDidMount = async () => {
+    const title = this.props.location.state.recipe;
+    const req = await fetch(
+      `https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=${title}`
+      //   `https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=${title}`
+    );
+    const res = await req.json();
+    this.setState({ activeRecipe: res.recipes[0] });
+  };
+
+  render() {
+    const recipe = this.state.activeRecipe;
+    return (
+      <div className="container">
+        {this.state.activeRecipe.length !== 0 && (
+          <div className="active-recipe">
+            <img
+              className="active-recipe__img"
+              src={recipe.image_url}
+              alt={recipe.title}
+            />
+            <h3 className="active-recipe__title">{recipe.title}</h3>
+            <h4 className="active-recipe__publisher">
+              Publisher: <span>{recipe.publisher}</span>
+            </h4>
+            <p className="acive-recipe__website">
+              Website:{" "}
+              <span>
+                <a href={recipe.publisher_url}>{recipe.publisher_url}</a>
+              </span>
+            </p>
+            <button className="active-recipe__button">
+              <Link to="/">Go Home</Link>
+            </button>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Recipe;
